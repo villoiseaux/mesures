d5f0934efb5780417061cb203637e35b3b196bdf
Jean PINON <github@jean.pinon.name>
2023-02-10 11:52:48 +0100

    multiple files management

--- graph.py
+++ graph.py
@@ -18,13 +18,13 @@ def import_text(filename, separator):
 parser = argparse.ArgumentParser(description='mesure grapher')
 
 
-parser.add_argument('filename', type=str, help='Name of file to graph')
+parser.add_argument('filename', type=str, help='Name of file(s) to graph', nargs='+')
 parser.add_argument('-x', dest='x', type=int, help='X column index')
 parser.add_argument('-y', dest='y', type=int, help='Y column index')
-parser.add_argument('-db', dest='gain',  action='store_true')
+parser.add_argument('-db', dest='gain',  action='store_true', help='Y is gain in dB 20 log10(Vs/Vi)')
 parser.add_argument('-s', dest='sec', type=int, help='Secondary Y column index')
-parser.add_argument('-xlog', dest='xlogscale',  action='store_true')
-parser.add_argument('-v', '--verbose', dest='verbose', action='store_true')  # on/off flag
+parser.add_argument('-xlog', dest='xlogscale',  action='store_true', help='display x using log scale')
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode')  # on/off flag
 
 args = parser.parse_args()
 
@@ -42,35 +42,39 @@ if (args.xlogscale is None):
 	args.xlogscale=0
 	
 	
-xValues=[]
-yValues=[]
-sValues=[]
-gValues=[]
 
-for data in import_text(args.filename, ','):
-	if (args.verbose):
-		print (data[args.x]+","+data[args.y])
-	xValues.append(float(data[args.x]))
-	yValues.append(float(data[args.y]))
-	if (args.sec):
-		sValues.append(float(data[args.sec]))
-		if (args.gain):
-			if ((float(data[args.y])>0) and (float(data[args.sec])>0)):
-				print (float(data[args.y]))
-				print (float(data[args.sec]))
-				gValues.append(math.log10(float(data[args.y])/float(data[args.sec])))
-			else:
-				xValues.pop()
+if (args.verbose):
+	print (args.filename)
 
-title=re.sub('\.csv$', '', args.filename)
-plt.figure(num=title)
+for file in args.filename:
+	xValues=[]
+	yValues=[]
+	sValues=[]
+	gValues=[]
+	for data in import_text(file, ','):
+		if (args.verbose):
+			print (data[args.x]+","+data[args.y])
+		xValues.append(float(data[args.x]))
+		yValues.append(float(data[args.y]))
+		if (args.sec):
+			sValues.append(float(data[args.sec]))
+			if (args.gain):
+				if ((float(data[args.y])>0) and (float(data[args.sec])>0)):
+					gValues.append(20*math.log10(float(data[args.y])/float(data[args.sec])))
+				else:
+					xValues.pop()
+					if (args.verbose):
+						print ("  <<Rejected>>")
 
-if (args.gain):
-	plt.plot(xValues,gValues)
-else:
-	plt.plot(xValues,yValues)
-	if (args.sec):
-		plt.plot(xValues,sValues)
+	title=re.sub('\.csv$', '', args.filename[0])
+	plt.figure(num=title)
+
+	if (args.gain):
+		plt.plot(xValues,gValues)
+	else:
+		plt.plot(xValues,yValues)
+		if (args.sec):
+			plt.plot(xValues,sValues)
 
 if (args.xlogscale):
 	plt.xscale('log',base=10) 
